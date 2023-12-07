f7f4eaf7c81114192c175538abc4fe6936ec9524
Lucas Iglesias <lucasiglesias2704@gmail.com>
2023-12-07 13:05:26 -0300

    Step 8 Completed

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,13 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
- ], (Controller, MessageToast, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+ ], (Controller, MessageToast, JSONModel, ResourceModel) => {
     "use strict";
  
     return Controller.extend("ui5.walkthrough.controller.App", {
        onInit() {
-          // set data model on view
           const oData = {
              recipient : {
                 name : "World"
@@ -15,10 +15,19 @@ sap.ui.define([
           };
           const oModel = new JSONModel(oData);
           this.getView().setModel(oModel);
+
+          const i18nModel = new ResourceModel({
+            bundleName: "ui5.walkthrough.i18n.i18n"
+          });
+          this.getView().setModel(i18nModel, "i18n");
        },
  
        onShowHello() {
-          MessageToast.show("Hello World");
+          const oBundle = this.getView().getModel("i18n").getResourceBundle();
+          const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+          const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+          MessageToast.show(sMsg);
        }
     });
  });
